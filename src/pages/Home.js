02fb901d44486107bd8b2e98bbaf27f2f5864569
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-14 02:29:29 -0300

    introduction

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 import BulboSimulacao from "../components/HomeContent";
+import RedirectButton from "../components/ButtonRedirect";
 
 function Home() {
   return (
     <>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h5>Realize Simulações do Bulbo Umido em Camada Superficial</h5>
+        <p>
+          Leia o conteudo abaixo para saber mais sobre Bulbo Umido em Camada
+          Superficial
+        </p>
+      </div>
+      <RedirectButton />
       <BulboSimulacao />
     </>
   );
