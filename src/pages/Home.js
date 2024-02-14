9f81d482206125759934bfc444dfe836f43cd02c
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-14 14:40:11 -0300

    scroll reveal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BulboSimulacao from "../components/HomeContent";
 import RedirectButton from "../components/ButtonRedirect";
+import ScrollReveal from "scrollreveal";
 
 function Home() {
+  useEffect(() => {
+    ScrollReveal().reveal("#intro", {
+      origin: "right",
+      distance: "20px",
+      duration: 1000,
+      delay: 300,
+      easing: "cubic-bezier(0.5, 0, 0, 1)",
+    });
+  }, []);
+
   return (
     <>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h5>Realize Simulações do Bulbo Umido em Camada Superficial</h5>
-        <p>
-          Leia o conteudo abaixo para saber mais sobre Bulbo Umido em Camada
-          Superficial
-        </p>
+      <div id="intro">
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h5>Realize Simulações do Bulbo Úmido em Camada Superficial</h5>
+          <p>
+            Leia o conteúdo abaixo para saber mais sobre Bulbo Úmido em Camada
+            Superficial
+          </p>
+        </div>
+        <RedirectButton />
       </div>
-      <RedirectButton />
       <BulboSimulacao />
     </>
   );
