0c1ae5213a3792e31746877c3a25927ea90e7078
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-14 14:58:18 -0300

    loading view

--- src/pages/Simulacoes.js
+++ src/pages/Simulacoes.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SelectorSimulation from "../components/SelectorSimulation";
+import ScrollReveal from "scrollreveal";
 
 function Simulacoes() {
+  useEffect(() => {
+    ScrollReveal().reveal("#selectorSimulationContainer", {
+      origin: "right",
+      distance: "20px",
+      duration: 1000,
+      delay: 300,
+      easing: "cubic-bezier(0.5, 0, 0, 1)",
+    });
+  }, []);
+
   return (
     <>
-      <SelectorSimulation />
+      <div id="selectorSimulationContainer">
+        <SelectorSimulation />
+      </div>
     </>
   );
 }
